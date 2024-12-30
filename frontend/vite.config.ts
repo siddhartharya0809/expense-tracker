fd477176a74b2339dd72a432ed997f4f4b52b7a1
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-12-30 18:59:42 +0530

    modified config files

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@preact/preset-vite";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
